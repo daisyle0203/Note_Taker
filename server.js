0b25bb477564e54e386e05e2c70e3d4cc02f0194
Daisy Le <daisy.le0203@gmail.com>
2022-10-18 01:26:06 -0500

    updated routers

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 // Require express
-const express = require('express');
+const express = require("express")
 // Require clog middleware
-const { clog } = require('./middleware/clog');
-// Set up link for router
-const routes = require('./routes/')
+const { clog } = require("./middleware/clog")
+// Set up link for routers
+const htmlRouter = require("./routes/html.js")
+const notesRouter = require("./routes/notes.js")
 
-// Call express 
-const app = express();
+// Call express
+const app = express()
 // Set up port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(clog)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
-// Set up static middleware
-app.use(express.static('public'));
-// Set up routes middleware
-app.use('/', routes);
+app.use("/", htmlRouter)
+app.use("/notes", notesRouter)
 
 // Server set to listen to PORT
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+)
